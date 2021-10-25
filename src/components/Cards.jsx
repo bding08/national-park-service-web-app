7768fcf94d533ebdc25dca062142c8f9df52a7bc
Brian Ding <bding08@gmail.com>
2021-10-24 20:22:21 -0400

    first commit

---
+++ src/components/Cards.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import CardItem from "./CardItem";
+import "./Cards.css";
+import img9 from "../images/img-9.jpg";
+import img2 from "../images/img-2.jpg";
+
+const Cards = () => {
+  return (
+    <div className="cards">
+      <h1>Explore these beautiful National Parks!</h1>
+      <div className="cards__container">
+        <div className="cards__wrapper">
+          <ul className="cards__items">
+            <CardItem
+              src={img9}
+              text="Explore the beautiful waterfalls"
+              label="Adventure"
+              path="/services"
+            />
+            <CardItem
+              src={img2}
+              text="Explore the beautiful waterfalls"
+              label="Adventure"
+              path="/services"
+            />
+          </ul>
+          <ul className="cards__items">
+            <CardItem
+              src={img2}
+              text="Explore the beautiful waterfalls"
+              label="Adventure"
+              path="/services"
+            />
+            <CardItem
+              src={img2}
+              text="Explore the beautiful waterfalls"
+              label="Adventure"
+              path="/services"
+            />
+            <CardItem
+              src={img2}
+              text="Explore the beautiful waterfalls"
+              label="Adventure"
+              path="/services"
+            />
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
